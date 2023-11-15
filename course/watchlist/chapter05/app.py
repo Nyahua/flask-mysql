82cfb5e700449a62b34257ac67ebcc4b8778e1db
nyahua <nyahua@outlook.com>
2023-11-15 23:56:01 +0800

    chapter 05

---
+++ course/watchlist/chapter05/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+from markupsafe import escape
+
+# movies = [
+#     {'title': 'My Neighbor Totoro', 'year': '1988'},
+#     {'title': 'Dead Poets Society', 'year': '1989'},
+#     {'title': 'A Perfect World', 'year': '1993'},
+#     {'title': 'Leon', 'year': '1994'},
+#     {'title': 'Mahjong', 'year': '1996'},
+#     {'title': 'Swallowtail Butterfly', 'year': '1996'},
+#     {'title': 'King of Comedy', 'year': '1999'},
+#     {'title': 'Devils on the Doorstep', 'year': '1999'},
+#     {'title': 'WALL-E', 'year': '2008'},
+#     {'title': 'The Pork of Music', 'year': '2012'},
+# ]
+
+from .model import Movie
+movies = []
+for instance in Movie.query.all():
+    movie = dict(title=instance.movie_name, year=instance.release_year)
+    movie['actors'] = [actor.actor_name for actor in instance.actors]
+    movies += [movie]
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+@app.route('/home')
+def index():
+    return render_template('tpl_index.html', movies=movies)
+
+@app.route('/user/<name>')
+def user_page(name):
+    return dict(User=escape(name))
